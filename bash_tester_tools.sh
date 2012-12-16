7817d4ac73c73503b44db02796caa17a36866c5a
Bruno Gonzalez <stenyak@stenyak.com>
2012-12-16 12:32:22 +0100

    Sourced tools variables using its own prefix to prevent name collisions.

--- bash_tester_tools.sh
+++ bash_tester_tools.sh
@@ -1,35 +1,34 @@
-#!bash_tester.sh
+#!/bin/bash
 # Copyright 2012 Bruno Gonzalez
 # This software is released under the GNU GENERAL PUBLIC LICENSE (see gpl-3.0.txt or www.gnu.org/licenses/gpl-3.0.html)
 
-results=""
-test_ret=0
-lastline=0
-filename="$1"
-function results()
+btt_results=""
+btt_test_ret=0
+btt_lastline=0
+btt_filename="$1"
+function btt_print_results()
 {
-    echo "======================================="
-    if [ "$results" == "" ]
+    if [ "$btt_results" == "" ]
     then
         echo "Ran 0 tests"
     else
-        echo "$results"
+        echo "$btt_results"
     fi
-    exit $test_ret
+    exit $btt_test_ret
 }
-function fail ()
+function btt_fail ()
 {
     local ret=$1 # error status
     local funcname="$3"
-    local line=$lastline # LINENO
+    local line=$btt_lastline # LINENO
     local command="$2"
     if [ "$funcname" != "" ]
     then
-        lastlineno=${BASH_LINENO[0]}
-        linenos=("${BASH_LINENO[@]}")
+        local lastlineno=${BASH_LINENO[0]}
+        local linenos=("${BASH_LINENO[@]}")
         unset linenos[0]
         command="$(cat $0 |head -n $lastlineno |tail -n 1 |sed "s/^\s*//g;")"
-        echo "$filename:$lastlineno: error $ret returned by command: '$command'"
+        echo "$btt_filename:$lastlineno: error $ret returned by command: '$command'"
         for lno in ${linenos[@]}
         do
             if [ "$lno" -ne "0" ]
@@ -41,25 +40,25 @@ function fail ()
         echo ""
     else
         command="$(cat $0 |head -n $line |tail -n 1)"
-        echo "$filename:$line: error $ret returned by command: '$command'"
+        echo "$btt_filename:$line: error $ret returned by command: '$command'"
         echo ""
     fi
 }
-function debug ()
+function btt_debug ()
 {
     local ret=$1 # error status
     local line=$2 # LINENO
     if [ "$ret" -eq 0 ]
     then
-        results="${results}."
-        lastline=$line
+        btt_results="${btt_results}."
+        btt_lastline=$line
     else
-        results="${results%?}F"
-        test_ret=1
+        btt_results="${btt_results%?}F"
+        btt_test_ret=1
     fi
 }
 set -o errtrace
-trap results 0
-trap 'fail $? "$BASH_COMMAND" $FUNCNAME'  ERR
-trap 'debug $? $LINENO "$BASH_COMMAND"'  debug
+trap btt_print_results 0
+trap 'btt_fail $? "$BASH_COMMAND" $FUNCNAME'  ERR
+trap 'btt_debug $? $LINENO "$BASH_COMMAND"'  debug
 
